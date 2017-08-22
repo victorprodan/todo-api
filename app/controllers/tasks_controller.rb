c7943faa0b3c5cdee7057a4647280761e8f4f061
prodanvictor <victor.prodan@luminos.software>
2017-08-22 17:24:08 +0300

    notifier disabled

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,18 +16,18 @@ class TasksController < ApplicationController
   def update
     task.update(status: status_params) if Task::StatusTransition.status_transition(task, status_params)
     render json: task, serializer: TaskSerializer
-    Task::NotificationSlack.notify_on_slack(task.id, "updated", task.description, task.status)
+  #  Task::NotificationSlack.notify_on_slack(task.id, "updated", task.description, task.status)
     sms_message = "You have updated the task with ID: #{task.id}. The description of the task is #{task.description} and its status is #{task.status}"
-    Task::SmsNotification.sms_notify(sms_message)
+  #  Task::SmsNotification.sms_notify(sms_message)
   end
 
   def destroy
     task = Task.find(params[:id])
     task.destroy
     render json: task, serializer: TaskSerializer
-    Task::NotificationSlack.notify_on_slack(task.id, "deleted", task.description, task.status)
+    # Task::NotificationSlack.notify_on_slack(task.id, "deleted", task.description, task.status)
     sms_message = "You have deleted the task with ID: #{task.id}. The description of the task is #{task.description} and its status is #{task.status}"
-    Task::SmsNotification.sms_notify(sms_message)
+    # Task::SmsNotification.sms_notify(sms_message)
   end
 
   def status_params
--- app/services/task/notification_slack.rb
+++ app/services/task/notification_slack.rb
@@ -2,7 +2,7 @@ class Task::NotificationSlack
 
   def self.notify_on_slack(task_id, action, task_data_now, task_status)
     notifier = Slack::Notifier.new 'https://hooks.slack.com/services/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' do
-      defaults channel: '#prodanvictor',
+      defaults channel: '@prodanvictor',
                 username: "TaskApp"
     end
       notifier.ping " You have #{action} task with ID: #{task_id}. The description of the task is: '#{task_data_now}' and its status is: #{task_status}"
